e7bb7c68b2c8784c7dcaff9cca87f44304f9b1bf
Ana-22 <61195614+Ana-22@users.noreply.github.com>
2020-03-09 16:57:08 +0100

    listbook

---
+++ app/src/main/java/com/example/readsapp/Activitys/ListsBooks.java
@@ -0,0 +1,17 @@
+package com.example.readsapp.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readsapp.R;
+
+public class ListsBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lists_books);
+    }
+
+}
