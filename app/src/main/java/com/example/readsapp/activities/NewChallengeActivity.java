6dc73a5b15d68217b14a6efb10bc0a628a009075
Pablo <pablo_f.i@hotmail.com>
2020-04-23 00:47:16 +0200

    Minor changes for Challenges creation

--- app/src/main/java/com/example/readsapp/activities/NewChallengeActivity.java
+++ app/src/main/java/com/example/readsapp/activities/NewChallengeActivity.java
@@ -3,6 +3,7 @@ package com.example.readsapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.readsapp.R;
 
@@ -12,5 +13,8 @@ public class NewChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_challenge);
+    }
+
+    public void createNewChallenge(View view) {
     }
 }
--- app/src/main/java/com/example/readsapp/models/Challenge.java
+++ app/src/main/java/com/example/readsapp/models/Challenge.java
@@ -1,13 +1,89 @@
 package com.example.readsapp.models;
 
 import java.util.Date;
-import java.util.List;
 
 public class Challenge {
-    int days;
-    Book book;
-    Date start;
-    Date finish;
-    List<Date> readingDays;
+    private Book book;
+    private Date start;
+    private Date finish;
+    private long pagesPerDay;
+    private long pagesRead;
+    private int percentageRead;
+    private boolean isActive;
 
+    public Challenge(Book book, Date start, Date finish) {
+        this.book = book;
+        this.start = start;
+        this.finish = finish;
+        pagesPerDay = (book.getVolumeInfo().getPageCount())/(daysBetween(start,finish));
+        pagesRead = 0;
+        percentageRead = 0;
+        isActive = true;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Date finish) {
+        this.finish = finish;
+    }
+
+    public long getPagesPerDay() {
+        return pagesPerDay;
+    }
+
+    public void setPagesPerDay(long pagesPerDay) {
+        this.pagesPerDay = pagesPerDay;
+    }
+
+    public long getPagesRead() {
+        return pagesRead;
+    }
+
+    public void setPagesRead(long pagesRead) {
+        this.pagesRead = pagesRead;
+    }
+
+    public int getPercentageRead() {
+        return percentageRead;
+    }
+
+    public void setPercentageRead(int percentageRead) {
+        this.percentageRead = percentageRead;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void increaseReading() {
+        pagesRead += pagesPerDay;
+        percentageRead += pagesRead/(book.getVolumeInfo().getPageCount());
+        if(pagesRead >= book.getVolumeInfo().getPageCount()) isActive = false;
+    }
+
+    private int daysBetween(Date d1, Date d2){
+        return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+    }
 }
